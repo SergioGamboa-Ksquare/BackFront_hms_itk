24ae6ac5cff0a4c1ff6f9dd2a5c2e2cc7e5d43d2
SergioGamboa-Ksquare <102177384+SergioGamboa-Ksquare@users.noreply.github.com>
2022-06-17 12:38:34 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop/index";
 
+import LoginIndex from "./pages/Auth/Login/index";
 import AboutIndex from "./pages/About/index";
 
 
-const App = () => {
-  return (
+@@ -11,6 +11,7 @@ const App = () => {
       <Router>
           <div className='App'>
           |<Route exact path="/" component={AboutIndex} />
+           <Route exact path="/login" component={LoginIndex} />
           </div>
       </Router>
   );
 };
-
-
-
 export default App;
