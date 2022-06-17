1086746020cfbe8c17317dcea555b1bb68b6636b
SergioGamboa-Ksquare <102177384+SergioGamboa-Ksquare@users.noreply.github.com>
2022-06-17 12:53:36 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import "./App.scss";
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import ScrollToTop from "./components/ScrollToTop/index";
-
 import LoginIndex from "./pages/Auth/Login/index";
 import AboutIndex from "./pages/About/index";
+import RegisterIndex from "./pages/Auth/Register/index";
 
 
-@@ -11,6 +11,7 @@ const App = () => {
+const App = () => {
+  return (
       <Router>
           <div className='App'>
           |<Route exact path="/" component={AboutIndex} />
            <Route exact path="/login" component={LoginIndex} />
+           <Route exact path="/register" component={RegisterIndex} />
           </div>
       </Router>
   );
